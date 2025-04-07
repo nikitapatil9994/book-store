9489155998747ce177b3870a0ff68ea7bf926de6
nikitapatil9994 <167848650+nikitapatil9994@users.noreply.github.com>
2025-04-07 11:05:17 +0530

    Add files via upload

---
+++ src/components/Book/Book.js
@@ -0,0 +1,35 @@
+import { Button } from "@mui/material";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import "./Book.css";
+const Book = (props) => {
+  const history = useNavigate();
+  const { _id, name, author, description, price, image } = props.book;
+  const deleteHandler = async () => {
+    await axios
+      .delete(`http://localhost:5000/books/${_id}`)
+      .then((res) => res.data)
+      .then(() => history("/"))
+      .then(() => history("/books"));
+  };
+
+  return (
+    <div className="card">
+      <img src={image} alt={name} />
+      <article>By {author}</article>
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <h2>Rs {price}</h2>
+      <div>
+      <Button LinkComponent={Link} to={`/books/${_id}`} sx={{ mt: "auto" }}  style={{backgroundColor:"blue",color:"white"}}>
+        Update
+      </Button>
+      <Button  onClick={deleteHandler} sx={{ mt: "auto" }} style={{backgroundColor:"red",color:"black"}}>
+        Delete
+      </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Book;
